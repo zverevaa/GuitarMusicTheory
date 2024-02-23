2412b9dc4e8d595f8972496ce905c75d461a7ae7
Kira Zvereva <angryvex@gmail.com>
2024-02-23 19:39:16 +0300

    Add views

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL)
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'GuitarView',
+            component: () => import('../views/GuitarView.vue')
+        },
+        {
+            path: '/eartrainer',
+            name: 'EarTrainerView',
+            component: () => import('../views/EarTrainerView.vue')
+        }
+    ]
 })
 
 export default router
