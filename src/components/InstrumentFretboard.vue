0cafa006c74721218e7aaec53e1f3de98248ad59
Kira Zvereva <angryvex@gmail.com>
2024-02-23 20:29:32 +0300

    Multiply strings

--- src/components/InstrumentFretboard.vue
+++ src/components/InstrumentFretboard.vue
@@ -1,9 +1,16 @@
 <template>
-    <div><InstrumentString /></div>
+    <div>
+        <InstrumentString v-for="(string, index) in stringTuning" :idx="index" :key="index" />
+    </div>
 </template>
 
 <script setup>
 import InstrumentString from './InstrumentString.vue'
+import { useStringsStore } from '@/stores/strings'
+
+const stringsStore = useStringsStore()
+
+const stringTuning = stringsStore.tuning
 </script>
 
 <style lang="scss" scoped></style>
