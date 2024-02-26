20c724cb81ca1f78565388ab7ecec3704937f8f9
Kira Zvereva <angryvex@gmail.com>
2024-02-26 05:26:19 +0300

    Add single note quiz

---
+++ src/components/SingleNoteQuiz.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="single-note-quiz">
+        <div class="notes">
+            <div class="note" v-for="note in stringsStore.notes" :key="note">{{ note }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useStringsStore } from '@/stores/strings'
+const stringsStore = useStringsStore()
+</script>
+
+<style lang="scss">
+.single-note-quiz {
+    display: flex;
+    justify-content: center;
+    .notes {
+        display: flex;
+        .note {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 20px;
+            background-color: #98c8e0;
+            width: 50px;
+            aspect-ratio: 1;
+            border: 2px solid black;
+            border-radius: 15%;
+        }
+    }
+}
+</style>
--- src/views/EarTrainerView.vue
+++ src/views/EarTrainerView.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>Ear training view</h1>
-  </div>
+    <div>
+        <h1>Ear training view</h1>
+        <InstrumentFretboard class="instrument" />
+        <SingleNoteQuiz />
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import InstrumentFretboard from '@/components/InstrumentFretboard.vue'
+import SingleNoteQuiz from '@/components/SingleNoteQuiz.vue'
+</script>
 
 <style lang="scss" scoped></style>
